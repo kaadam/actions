d31d001f1aedffd365c284ce639ecdf38726768c
Adam Kallai <kadam@inf.u-szeged.hu>
2023-03-13 14:29:52 +0100

    Add support for setting Host architecture via vsdevcmd

--- setup-windows/main.js
+++ setup-windows/main.js
@@ -1,12 +1,17 @@
 const { exec } = require('child_process');
 const path = require('path');
 
-if (process.argv.length != 2) {
+if (process.argv.length > 3) {
   console.error("usage: process.argv[0] process.argv[1]");
   process.exit(1);
 }
 
 var arch = process.env.INPUT_ARCH.toLowerCase()
+var args = `'-arch=${arch}'`
+if (process.env.INPUT_HOST_ARCH) {
+  var host = process.env.INPUT_HOST_ARCH.toLowerCase()
+  args = args.concat(' ', `'-host_arch=${host}'`)
+}
 var script = path.join(__dirname, 'vs_setup.bat')
 
 cmd = 'git config --global core.autocrlf false'
@@ -19,7 +24,7 @@ exec(cmd, (error, stdout, stderr) => {
 });
 
 console.log(`${script}`)
-exec(`${script} -arch=${arch}`, (error, stdout, stderr) => {
+exec(`${script} ${args}`, (error, stdout, stderr) => {
 
   console.error(`${stderr}`);
   if (error) {
